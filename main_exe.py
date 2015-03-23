b4b44ecf4a812337f7574a6998c1c02e84fe6ad9
LFonte <l.fonte@hotmail.com>
2015-03-23 22:15:39 +0000

    Teste commit com clone

---
+++ main_exe.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Main File
+
+@author Luis Fonte
+@date 20150307
+"""
+print '\n---------------------------------------------------'
+print '---------- Network Security Application -----------'
+print '---------------------------------------------------'
+print '--------------- Author: Luis Fonte ----------------'
+print '----------------- LPD MESI IPBEJA -----------------'
+print '---------------------------------------------------'
+
+
+
+def zero():
+    print 'teste1'
+    pass
+
+def um():
+    print 'teste2'
+    pass
+
+def dois():
+    print 'teste3'
+    pass
+
+def tres():
+    print 'teste4'
+    pass
+
+
+options = {0 : zero,
+           1 : um,
+           2 : dois,
+           3 : tres} 
+
+options[2]()
+
+    
